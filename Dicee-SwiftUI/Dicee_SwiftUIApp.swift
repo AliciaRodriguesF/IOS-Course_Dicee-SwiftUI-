8d6330bdcb8b03c7779ade0b5c93b6007b51e523
AliciaRodrigues <rodriguesf.alicia@gmail.com>
2022-04-13 11:54:06 -0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Alicia Rodrigues  on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
